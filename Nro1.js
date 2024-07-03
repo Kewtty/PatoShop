ccd631e07178bf134a6809d41303d4c115a8de22
Kewtty <202301647@est.umss.edu>
2024-07-03 16:16:24 -0400

    first commit

---
+++ Nro1.js
@@ -0,0 +1,17 @@
+console.log("wero")
+
+let a, b, c;
+a = 1;
+b = 9;
+c = a+b;
+console.log(c)
+
+if(c == 6){
+    console.log("VAMOOOOOOOOO")
+}else{
+    console.log("CHISSS")
+}
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
---
+++ script.js
@@ -0,0 +1,16 @@
+const b = document.getElementById("botonPato");
+b.addEventListener('click',function (){
+    alert("Hola Pato")
+})
+
+
+/*b.addEventListener('click', ()=> {
+    alert("Hola Pato");
+})
+
+function Pato(){
+    alert("Hola Pato");
+}
+
+const b = document.getElementById("botonPato");
+b.addEventListener('click', Pato)/*/
