5d98a9c8d3a932c3a2f244bb1fcebe8ad4f5817b
Kewtty <202301647@est.umss.edu>
2024-07-08 16:57:41 -0400

    Boton de modo oscuro para el index, texto en blanco y transicion patuna.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let b = document.getElementById("botonPato");
+let c = document.getElementById("ModOscuro");
 /*b.addEventListener('click',function (){
     alert("Hola Pato")
 })*/
 b.addEventListener('click',function (){
     window.location.href = 'mainpage.html'
+})
+
+c.addEventListener('click', function (){
+    document.body.classList.toggle("modoOscuro")
 })
